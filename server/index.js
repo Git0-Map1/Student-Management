d3f6c6db9c14661f450acc981ffe49664a065f5c
Git0-Map1 <git0map0@gmail.com>
2026-07-07 20:31:59 +0545

    initial commit

---
+++ server/index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+
+const app = express();
+
+//every request passes through middleware before reaching your routes 
+app.use(express.json());
+
+const PORT = 5000;
+
+// app.get("/", (req, res) =>{
+//     res.json({
+//         message: "Hello from my backend!"
+//     });
+// })
+
+let students = [
+    { id: 1, name: "John" },
+    { id: 2, name: "Alice" },
+    { id: 3, name: "Mausam" }
+];
+
+app.get("/students", (req, res) => {
+    res.json(students);
+});
+
+app.get("/", (req,res) =>{
+    res.send("hello from backend");
+});
+
+// app.post("/students", (req, res) => {
+//     console.log(req.body);
+
+//     res.json({
+//         message: "Student received!"
+//     });
+// });
+
+app.post("/students", (req, res) => {
+    const newStudent = {
+        id: students.length + 1,
+        name: req.body.name
+    };
+
+    students.push(newStudent);
+
+    res.json({
+        message: "Student added successfully!",
+        student: newStudent
+    });
+});
+
+app.put("/students/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    const student = students.find(student => student.id === id);
+
+    if (!student) {
+        return res.status(404).json({
+            message: "Student not found"
+        });
+    }
+
+    student.name = req.body.name;
+
+    res.json(student);
+});
+
+app.delete("/students/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    students = students.filter((student) => student.id !== id);
+
+    res.json(students);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});

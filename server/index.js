7ec35ed84d86236e92bc48ef2f49094d7e4170f3
Git0-Map1 <git0map0@gmail.com>
2026-07-13 14:55:34 +0545

    CRUD operations completion

--- server/index.js
+++ server/index.js
@@ -1,32 +1,34 @@
+
 const express = require("express");
 const cors = require("cors");
+const { PrismaClient } = require("@prisma/client");
 
 
 const app = express();
-
+const prisma = new PrismaClient();
 app.use(cors())
 //every request passes through middleware before reaching your routes 
 app.use(express.json());
 
 const PORT = 5000;
 
-// app.get("/", (req, res) =>{
-//     res.json({
-//         message: "Hello from my backend!"
-//     });
-// })
+// let students = [
+//     { id: 1, name: "John" },
+//     { id: 2, name: "Alice" },
+//     { id: 3, name: "Mausam" }
+// ];
 
-let students = [
-    { id: 1, name: "John" },
-    { id: 2, name: "Alice" },
-    { id: 3, name: "Mausam" }
-];
+// app.get("/students", (req, res) => {
+//     res.json(students);
+// });
 
-app.get("/students", (req, res) => {
+app.get("/students", async (req, res) => {
+    const students = await prisma.student.findMany();
+
     res.json(students);
 });
 
-app.get("/", (req,res) =>{
+app.get("/", (req, res) => {
     res.send("hello from backend");
 });
 
@@ -39,49 +41,115 @@ app.get("/", (req,res) =>{
 // });
 
 
-app.post("/students", (req, res) => {
+// app.post("/students", (req, res) => {
 
-    if(!req.body.name){
+//     if(!req.body.name){
+//         return res.status(400).json({
+//             message:"name field must not be empty"
+//         })
+//     }
+//     const newStudent = {
+//         id: students.length + 1,
+//         name: req.body.name
+//     };
+
+//     students.push(newStudent);
+
+//     res.json({
+//         message: "Student added successfully!",
+//         student: newStudent
+//     });
+// });
+
+app.post("/students", async (req, res) => {
+try{
+
+    if (!req.body.name) {
         return res.status(400).json({
-            message:"name field must not be empty"
-        })
+            message: "Name is required"
+        });
     }
-    const newStudent = {
-        id: students.length + 1,
-        name: req.body.name
-    };
     
-    students.push(newStudent);
-
-    res.json({
-        message: "Student added successfully!",
-        student: newStudent
+    const newStudent = await prisma.student.create({
+        data: {
+            name: req.body.name
+        }
     });
-});
+    res.json(newStudent)
+}
+catch{
+    res.status(500).json({
+        message: "Something went wrong"
+    })
+}
+})
+app.put("/students/:id", async (req, res) => {
+    try {
 
-app.put("/students/:id", (req, res) => {
-    const id = Number(req.params.id);
+        const updatedStudent = await prisma.student.update({
 
-    const student = students.find(student => student.id === id);
+            where: {
+                id: Number(req.params.id)
+            },
 
-    if (!student) {
-        return res.status(404).json({
-            message: "Student not found"
+            data: {
+                name: req.body.name
+            }
+
+        });
+        res.json(updatedStudent)
+    }
+    catch (error) {
+
+        res.status(404).json({
+            message: "Something went wrong"
         });
     }
 
-    student.name = req.body.name;
-
-    res.json(student);
 });
 
-app.delete("/students/:id", (req, res) => {
-    const id = Number(req.params.id);
+// app.put("/students/:id", (req, res) => {
+//     const id = Number(req.params.id);
 
-    students = students.filter((student) => student.id !== id);
+//     const student = students.find(student => student.id === id);
 
-    res.json(students);
+//     if (!student) {
+//         return res.status(404).json({
+//             message: "Student not found"
+//         });
+//     }
+
+//     student.name = req.body.name;
+
+//     res.json(student);
+// });
+
+app.delete("/students/:id", async (req, res) => {
+    try {
+
+        const deletedStudent = await prisma.student.delete({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+
+        res.json(deletedStudent);
+    }
+    catch (error) {
+        res.status(500).json({
+            message: "Something went wrong"
+        })
+    }
+
 });
+
+// app.delete("/students/:id", (req, res) => {
+//     const id = Number(req.params.id);
+
+//     students = students.filter((student) => student.id !== id);
+
+//     res.json(students);
+// });
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
